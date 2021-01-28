91e27aa9c31d7ed4af8f328c10ef597aedbc576b
Logesh Varathan <lvaradhan@zuora.com>
2021-01-28 11:24:12 +0530

    Right to bill Flag and Rc_id !=0
    Added Unbilled flag and included delinked RC's also in the report as requested.

--- PTC/SND01/Report/Custom Revenue Insight Report/RPRO_RC_LINE_CBILL_V.sql
+++ PTC/SND01/Report/Custom Revenue Insight Report/RPRO_RC_LINE_CBILL_V.sql
@@ -11,24 +11,25 @@ WITH bill AS( SELECT  rc_id
                       ,line_id )
 SELECT /* LEADING(rrl,rrb)*/
         rrl.id 
-       ,rrl.doc_date                  sales_order_date 
-       ,rrl.doc_num                   sales_order_num 
-       ,rrl.rc_id                     
-       ,rrl.item_num                  item_number 
-       ,rrl.atr9                      project_number 
-       ,rrl.doc_line_id               sales_order_line_id 
-       ,rrl.atr1 mson 
-       ,rrl.end_date 
-       ,rrl.start_date 
-       ,rrl.business_unit 
-       ,rrl.atr3                      subscription_number 
-       ,rrl.net_sll_prc               t_transaction_price 
-       ,rrl.net_sll_prc*rrl.f_ex_rate f_transaction_price 
-       ,rrl.net_sll_prc*rrl.g_ex_rate r_transaction_price 
+       ,rrl.doc_date                                     sales_order_date 
+       ,rrl.doc_num                                      sales_order_num 
+       ,rrl.rc_id                                        
+       ,rrl.item_num                                     item_number 
+       ,rrl.atr9                                         project_number 
+       ,rrl.doc_line_id                                  sales_order_line_id 
+       ,rrl.atr1 mson                                    
+       ,rrl.end_date                                     
+       ,rrl.start_date                                   
+       ,rrl.business_unit                                
+       ,rrl.atr3                                         subscription_number 
+       ,rrl.net_sll_prc                                  t_transaction_price 
+       ,rrl.net_sll_prc*rrl.f_ex_rate                    f_transaction_price 
+       ,rrl.net_sll_prc*rrl.g_ex_rate                    r_transaction_price 
+       ,rpro_rc_line_pkg.get_unbill_flag(rrl.indicators) right_to_bill_flag
 FROM rpro_rc_line_g rrl ,
      bill rrb
 WHERE rrb.line_id(+) = rrl.id
 AND   rrb.rc_id(+)   = rrl.rc_id
-AND   rrl.rc_id     != 0;
+AND   rrl.rc_id     != 1;
 
 /
